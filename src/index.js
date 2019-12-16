c37a9df41005722d1a4bb7b2f9eeccdeb5f7af1b
Bathisaikrishna <bathisaikrishna.d@gmail.com>
2019-12-16 13:14:32 +0530

    evencounter

--- src/index.js
+++ src/index.js
@@ -37,5 +37,34 @@ class Reloader extends React.Component {
     );
   }
 }
+
+class EvenCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicks: 0 };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+  clickHandler(event) {
+    this.setState({ clicks: this.state.clicks + 1 });
+    if (this.state.clicks % 2 === 0) {
+      this.props.onEvenClick(this.state.clicks);
+    }
+  }
+  render() {
+    return (
+      <div onClick={this.clickHandler}>
+        This Component clicked {this.state.clicks} times.
+      </div>
+    );
+  }
+}
 ReactDOM.render(<Events />, document.getElementById("root"));
 ReactDOM.render(<Reloader />, document.getElementById("root"));
+ReactDOM.render(
+  <EvenCounter
+    onEvenClick={data => {
+      console.log(data);
+    }}
+  />,
+  document.getElementById("root")
+);
