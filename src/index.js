882e89adaea8f07f1bc1e80bdd213628ba5d09be
Bathisaikrishna <bathisaikrishna.d@gmail.com>
2019-12-16 12:51:02 +0530

    reloader

--- src/index.js
+++ src/index.js
@@ -3,11 +3,39 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function Events(props){
+function Events(props) {
   const clickHandler = console.log();
-  return(<button onClick={clickHandler}>Make event</button>);
+  return <button onClick={clickHandler}>Make event</button>;
 }
 
-
-ReactDOM.render(<Events/>, document.getElementById("root"));
-ReactDOM.render(<Reloader/>, document.getElementById("roo"))
+class Reloader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { content: "" };
+    this.submitForm = this.submitForm.bind(this);
+    this.changeChar = this.changeChar.bind(this);
+  }
+  submitForm(event) {
+    if (this.state.content !== "reload") {
+      console.log(this.state.content);
+      event.preventDefault();
+    }
+  }
+  changeChar(event) {
+    this.setState({ content: event.target.value });
+  }
+  render() {
+    return (
+      <form onSubmit={this.submitForm}>
+        <input
+          type="text"
+          value={this.state.content}
+          onChange={this.changeChar}
+        />
+        <input type="Submit" value="Submit" />
+      </form>
+    );
+  }
+}
+ReactDOM.render(<Events />, document.getElementById("root"));
+ReactDOM.render(<Reloader />, document.getElementById("root"));
