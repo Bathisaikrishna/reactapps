1f85055c57d7a23c68d1de412fa6f0eb65c9a0fa
Bathisaikrishna <bathisaikrishna.d@gmail.com>
2019-12-27 17:14:30 +0530

    todolist

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
 
 function Events(props) {
@@ -58,13 +57,96 @@ class EvenCounter extends React.Component {
     );
   }
 }
-ReactDOM.render(<Events />, document.getElementById("root"));
-ReactDOM.render(<Reloader />, document.getElementById("root"));
-ReactDOM.render(
+//ReactDOM.render(<Events />, document.getElementById("root"));
+//ReactDOM.render(<Reloader />, document.getElementById("root"));
+/*ReactDOM.render(
   <EvenCounter
     onEvenClick={data => {
       console.log(data);
     }}
   />,
   document.getElementById("root")
-);
+);*/
+
+class Heading extends React.Component {
+  render() {
+    return <h1>{this.props.name}</h1>;
+  }
+}
+class Task extends React.Component {
+  render() {
+    return (
+      <li>
+        <input
+          type="checkbox"
+          value={this.props.done === true ? "checked" : ""}
+        />
+        {this.props.taskname}
+      </li>
+    );
+  }
+}
+
+class TaskContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.props.tasks.map(task => (
+            <Task key={task.id} {...task} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Form extends React.Component {
+  state = { desc: "" };
+  handleSubmit = event => {
+    event.preventDefault();
+    //console.log(this.state.desc);
+    this.props.callback(this.state.desc);
+    console.log("in handle submit");
+  };
+  handleChange(event) {
+    this.setState({ desc: event.target.value });
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.desc}
+          onChange={event => this.setState({ desc: event.target.value })}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+class ToDoApp extends React.Component {
+  jobs = 0;
+  tasks = [{ name: "task1", id: this.jobs++, done: true }];
+
+  myCallBack = desc => {
+    const task = { name: desc, id: this.jobs++, done: false };
+    this.tasks.push(task);
+    console.log(`${this.tasks[0].name}`);
+    console.log(`${this.tasks[1].name}`);
+    console.log(`in callback ${task.name}, ${task.id}`);
+  };
+  render() {
+    return (
+      <div>
+        <Heading name="ToDoList App" />
+        <TaskContainer tasks={this.tasks} />
+        <Form callback={this.myCallBack} />
+      </div>
+    );
+  }
+}
+render();
+function render() {
+  ReactDOM.render(<ToDoApp />, document.getElementById("root"));
+}
